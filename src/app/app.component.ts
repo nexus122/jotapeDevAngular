d19f514bd29ea45f2de4c812fd0e7c4b6f594386
nexus122 <juan-pablo122@hotmail.es>
2023-03-11 13:21:51 +0100

    Boton para subir arriba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jotapeDev';
+  scrollToTop() {
+    const scrollToTop = window.setInterval(() => {
+      const pos = window.pageYOffset;
+      if (pos > 0) {
+        window.scrollTo(0, pos - 20);
+      } else {
+        window.clearInterval(scrollToTop);
+      }
+    }, 16);
+  }
 }
